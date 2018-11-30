6afbc65447491f1209c556e4b2006615e56f91c9
Loan Laux <contact@loanlaux.fr>
2018-11-30 17:18:32 +0100

    Add footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 
@@ -12,6 +13,7 @@ const styles = (theme) => ({
     display: "flex",
     justifyContent: "space-between",
     paddingBottom: theme.spacing.unit * 2,
+    marginTop: "2rem",
     width: "100%"
   },
   row: {
@@ -21,18 +23,47 @@ const styles = (theme) => ({
     position: "absolute",
     left: "50%",
     transform: "translateX(-50%)"
+  },
+  linkList: {
+    listStyle: "none"
+  },
+  linkListItem: {
+    margin: ".25rem 0"
+  },
+  link: {
+    color: theme.palette.secondary.dark,
+    fontFamily: theme.typography.fontFamily,
+    textDecoration: "none",
+    "&:hover": {
+      color: theme.palette.primary.dark,
+      textDecoration: "underline"
+    }
+  },
+  contactColumn: {
+    color: theme.palette.secondary.dark,
+    fontFamily: theme.typography.fontFamily
   }
 });
 
 const Footer = ({ ...props }) => (
   <footer className={props.classes.footer}>
     <div className={props.classes.row}>
-      <ul>
-        <li><a href="#">About us</a></li>
-        <li><a href="#">Terms of Use</a></li>
-        <li><a href="#">Legal Terms</a></li>
-        <li><a href="#">Shipping</a></li>
-        <li><a href="#">Returns</a></li>
+      <ul className={props.classes.linkList}>
+        <li className={props.classes.linkListItem}>
+          <a className={props.classes.link} href="#">About us</a>
+        </li>
+        <li className={props.classes.linkListItem}>
+          <a className={props.classes.link} href="#">Terms of Use</a>
+        </li>
+        <li className={props.classes.linkListItem}>
+          <a className={props.classes.link} href="#">Legal Terms</a>
+        </li>
+        <li className={props.classes.linkListItem}>
+          <a className={props.classes.link} href="#">Shipping</a>
+        </li>
+        <li className={props.classes.linkListItem}>
+          <a className={props.classes.link} href="#">Returns</a>
+        </li>
       </ul>
     </div>
     <div className={props.classes.centerRow}>
@@ -40,7 +71,7 @@ const Footer = ({ ...props }) => (
         &copy; {date.getFullYear()} The Backpack Shop
       </Typography>
     </div>
-    <div className={props.classes.row}>
+    <div className={classNames([props.classes.row, props.classes.contactColumn])}>
       <p>You can reach our support at (123) 456-7890, Monday to Saturday, 8AM EST to 9PM EST.</p>
       <p>Send any question or suggestion to contact@thebackpackshop.com.</p>
       <p>We are based out of 123 Park Avenue, New York City, NY.</p>
